a97a5a2a821127f132fa07f034d95aaf2de62e59
Areeba Tariq <areebatariq885@gmail.com>
2024-11-30 17:55:46 +0500

    Add Column Names

--- SVM.py
+++ SVM.py
@@ -4,7 +4,20 @@ train_Data=pd.read_csv("./KDDTrain+.txt")
 
 pd.set_option('display.max_columns',None)
 
-prin(train_Data)
+# print(train_Data)
 
+# print(train_Data.columns)
+
+columns = (['duration','protocol_type','service','flag','src_bytes','dst_bytes','land','wrong_fragment','urgent'
+            ,'hot','num_failed_logins','logged_in','num_compromised','root_shell','su_attempted','num_root'
+            ,'num_file_creations','num_shells','num_access_files','num_outbound_cmds','is_host_login'
+            ,'is_guest_login','count','srv_count','serror_rate','srv_serror_rate','rerror_rate','srv_rerror_rate'
+            ,'same_srv_rate','diff_srv_rate','srv_diff_host_rate','dst_host_count','dst_host_srv_count'
+            ,'dst_host_same_srv_rate','dst_host_diff_srv_rate','dst_host_same_src_port_rate'
+            ,'dst_host_srv_diff_host_rate','dst_host_serror_rate','dst_host_srv_serror_rate','dst_host_rerror_rate'
+            ,'dst_host_srv_rerror_rate','attack','outcome'])
+
+train_Data.columns=columns
+print(train_Data)
 
 
